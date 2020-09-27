b3ac2f07b8032c9897c8d7cc5db786fbd2d1fd32
kikusourabh <karmakarsourabh7@gmail.com>
2020-09-27 18:28:31 +0530

    structure

---
+++ src/config/COLORS.js
@@ -0,0 +1,8 @@
+const COLORS = {
+  primary: '#201E2A',
+  secondary: '#A6A9B2',
+  grey: '#EFF0F2',
+  acent: '#FFFFFF',
+};
+
+export default COLORS;
---
+++ src/config/STYLES.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+import COLORS from './COLORS';
+
+const STYLES = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: COLORS.grey,
+  },
+});
+
+export default STYLES;
